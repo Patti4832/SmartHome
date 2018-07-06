3010af2c35f83c2b17f44a4c026be9db141697a9
Patti4832 <patti4832@gmx.de>
2018-07-06 11:52:19 +0200

    Create HA_Settings.cs

---
+++ Source/HA_Settings.cs
@@ -0,0 +1,15 @@
+namespace HA_Settings
+{
+    public class Settings
+    {
+        public int version = 1;
+
+        public int serverPort = 1241;
+        public string token = "";
+
+        public void Init()
+        {
+
+        }
+    }
+}
